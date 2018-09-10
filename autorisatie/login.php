7c091650f4458c6c6fa85222d7f10f7e9c01256a
Guido Leen <guido.leen@hva.nl>
2018-09-10 17:10:35 +0200

    Alle css in header

---
+++ autorisatie/login.php
@@ -0,0 +1,14 @@
+<?php
+    include ("../header/header.php");
+?>
+
+<!-- HIER KOMT DE OPMAAK!!!!! -->
+<form>
+    <label class="form-lbl">Gebruiknaam</label>
+    <input type="text" class="form-txt">
+
+</form>
+
+<!-- END OPMAAK!!!!! -->
+
+<?php include ("../footer/footer.php"); ?>
