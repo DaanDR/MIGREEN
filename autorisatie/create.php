4e64ceeb29228d3dae7f2f6fbb0a8aac19c9c2b5
Bobby Komen <bobby.komen@hva.nl>
2018-09-11 15:50:58 +0200

    Pagina gemaakt voor het aanmaken van gebruikers (grafisch)

---
+++ autorisatie/create.php
@@ -0,0 +1,35 @@
+<?php
+    include ("../header/header.php");
+?>
+
+    <link type="text/css" rel="stylesheet" href="../css/create.css">
+
+<body>
+    
+    <div class="menu">
+        <div id = "title">
+            MyInsight <br>
+        </div>
+        
+        <div id = "paragraaf">
+        <p> Voor hier een nieuwe gebruiker in: </p>
+        </div>
+        
+        <div class="user_input">
+            <form method="post">
+            <ul>
+            Klantnaam:      <input type="text" name="waarde" minlength=2>  <br> <br>
+            Wachtwoord:     <input type="password" name="waarde">  <br>
+                
+            <div class = "submit_button">
+                <input type="submit" value="aanmaken nieuwe klant">
+            </div>
+            </ul> 
+            </form>
+            
+        </div>
+    </div>        
+    
+
+<?php include ("../footer/footer.php"); ?>
+</body>
