25e8805f89b66f41f116614c315a7c11d10ca848
Daan de Rond <daanderond@hotmail.com>
2018-09-11 14:20:36 +0200

    dropdown veranderd

--- header/header.php
+++ header/header.php
@@ -1,4 +1,41 @@
- <?php
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+
+    <link type="text/css" rel="stylesheet" href="../css/core.css">
+    <link type="text/css" rel="stylesheet" href="../css/header.css">
+    <link type="text/css" rel="stylesheet" href="../css/formulier.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <script src="../js/header.js"></script>
+    
+
+
+    <title><?php echo $title ?></title>
+</head>
+<body>
+
+   <h1>MyInsights</h1>
+    <nav id="navmenu">
+        <ul>
+            <li><a href="http://localhost:8080/MIGreen/header/header.php" ></a><i class="fas fa-home"></i></li>
+            <li><a href="blabla" class="active">Gebruikers</a></li>
+            <li><a href="blabla">Klanten</a></li>
+            <li><a href="blabla">Omgevingen</a></li>
+<!--            <li><a href="dropdown"><i class="fas fa-user"> </a></li>-->
+        </ul>
+    </nav>
+         
+    <div id="usericon" class="dropdown">
+        <div id="usericonbar"><i class="fas fa-user"></i></div>
+        <div class="dropdown-content"><a href="header.php?action=account"> Account </a>
+        <a href="header.php?action=logout"> Uitloggen </a></div>
+    </div>
+    
+     <?php
 //  Voor de title bar
     session_start();
     if ( isset($_SESSION["title"]) )
@@ -9,40 +46,34 @@
     {
         $title = "MyBit MyInsight";
     }
+        
+//        $action = $_GET["action"];
+        
+    if (!isset($_GET["action"])){
+        $action = "Home";
+    }
+    else {
+        $action = $_GET["action"];
+    }
+
+    switch($action){
+            case"account": 
+            account();
+            break; 
+            case"logout":
+            logout();
+            break;
+    }
+//    function logout(){
+////        print "uitloggen";
+//       alert("Log uit bro");
+//    }
+        
+        function account(){
+            print "accountje";
+        }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-
-    <link type="text/css" rel="stylesheet" href="../css/core.css">
-    <link type="text/css" rel="stylesheet" href="../css/header.css">
-    <link type="text/css" rel="stylesheet" href="../css/navmenu.css">
-    <link type="text/css" rel="stylesheet" href="../css/formulier.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
-
-    <title><?php echo $title ?></title>
-</head>
-<body>
-
-  <nav id="navmenu">
-        <ul>
-            <li> <i class="fas fa-home"></i></li>
-            <li><a href="blabla" class="active">Gebruikers</a></li>
-            <li><a href="blabla">Klanten</a></li>
-            <li><a href="blabla">Omgevingen</a></li>
-        </ul>
-    </nav>
-         
-    <div id="usericon" class="dropdown">
-        <div id="usericonbar"></div>
-        <div id="usericonbar"></div>
-        <div id="usericonbar"></div>
-        <div class="dropdown-content">
-        <a href="account.html"> Account </a>
-        <a href="uitloggen.html"> Uitloggen </a>
-        </div>
-    </div>
+    
+    </body>
+    
+</html>
