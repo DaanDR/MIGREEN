1e22326c614c93d5c23aa0a77c6b9f0c35aa6677
Ilona Bruggeman <imbruggeman@live.nl>
2018-09-13 09:11:28 +0200

    revert of login and updated userdaomysql

---
+++ autorisatie/UserDaoMysql.php
@@ -0,0 +1,103 @@
+<?php
+
+// Default Connector voor de mysql...
+include_once ("../dbconnection/mysqlConnector.php");
+include_once ("UserDao.php");
+include_once ("User.php");
+
+class UserDaoMysql implements UserDao
+{
+    // private $usr = new User();
+    private $dbConn;
+
+    public function __construct()
+    {
+    }
+
+    // Insert new User
+    public function insertUser($userName, $password, $firstname, $lastname, $email, $role)
+    {
+        $dbConn = new mysqlConnector();
+
+        $sql = "INSERT INTO user(userName, password, firstname, lastname, email, role) VALUES (?, ?, ?, ?, ?, ?)";
+  
+        $stmt = $dbConn->getConnector()->prepare($sql);
+        $stmt->bind_param('ssssss', $userName, $password, $firstname, $lastname, $email, $role);
+        $stmt->execute();
+        
+        $dbConn->getConnector()->close();
+    }
+
+    public function updateUser($id)
+    {
+
+    }
+    
+    public function deleteUser($id)
+    {
+
+    }
+
+    public function selectUser($username)
+    {
+        $newUser = null;
+        $dbConn = new mysqlConnector();
+
+        $userid;
+        $userName;
+        $password;
+        $firstname;
+        $lastname;
+        $email;
+        $role;
+        
+        $sql = "SELECT userID, userName, password, firstname, lastname, email, role FROM user WHERE userName = ?"; 
+        $conn = $dbConn->getConnector();
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $stmt->store_result();    
+    
+        //checken of de sql statement een resultset teruggeeft (hij is leeg als de user niet bestaat)
+        if ($stmt->num_rows > 0)
+        {    
+        
+		$stmt->bind_result(
+            $userid,
+            $userName,
+            $password,
+            $firstname,
+            $lastname,
+            $email,
+            $role
+        );
+            // Vul de rij met maar 1 record uit de database
+            while ($stmt->fetch()) 
+            {
+                $newUser = new User($userid, $userName, $password, $firstname, $lastname, $email, $role);
+            }
+        }else
+        {
+            //alles op null zetten bij teruggave lege resultset
+            $userid = null;
+            $userName = null;
+            $password = null;
+            $firstname = null;
+            $lastname = null;
+            $email = null;
+            $role = null;
+            $newUser = new User($userid, $userName, $password, $firstname, $lastname, $email, $role);
+        }
+        
+        
+        return $newUser;
+    }
+    
+    
+    public function selectAllUsers()
+    {
+        
+    }
+}
+
+?>
