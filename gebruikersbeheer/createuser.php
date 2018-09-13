da0efee83471f247bdce90bcd7660ac004952c8b
anderson petrus <anderson.petrus@hva.nl>
2018-09-13 13:25:55 +0200

    updated creatuser.php en createuser.html

--- gebruikersbeheer/createuser.php
+++ gebruikersbeheer/createuser.php
@@ -22,46 +22,98 @@
 
 ?>
 
-<<<<<<< HEAD
-    <link type="text/css" rel="stylesheet" href="../css/content.css">
->>>>>>> development
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 
-<body>
-    
-    <div class="menu">
-        <div id = "title">
-            MyInsight <br>
-        </div>
+<head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/form.css">
+    <link rel="stylesheet" href="../css/content.css">
+    <link rel="stylesheet" href="../css/header.css">
+
+    <meta charset="utf-8">
+    <title>Gebruiker Aanmaken</title>
+</head>
+
+<div class="grid-container">
+
+    <div class="header-left">
+        <p>Home <i id="triangle-breadcrumb" class="fas fa-caret-right"></i> Gebruikersoverzicht</p>
+        <h1>Gebruiker aanmaken</h1>
+    </div>
+
+
+    <div class="header"></div>
+
+    <!-- form elements -->
+
+    <div class="content">
 
         <form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF'];?>">
-            <div class="userinformation">
-                <ul>
-        Gebruikersnaam  (minimaal: 5 karakters)                         <input type="text" name="username" minlength=5>       <br>
-        Wachtwoord (minimaal: 8 karakters, 1 Hoofdletter, 1 Nummer)     <input type="password" name="password">             <br>
-        Wachtwoord herhaling  (Moet gelijk zijn aan bovenliggend veld)  <input type="password" name="password">             <br>   Voornaam   (minimaal: 2 karakters)                               <input type="text" name="firstname" minlength=2>     <br>
-        Achternaam  (minimaal: 2 karakters)                             <input type="text" name="lastname" minlength=2>       <br>
-        Email (moet voldoen aan standaard email eisen - RFC 1035)       <input type="email" name="email">                  <br>
-        Rol (verplichte keuze)  <select name="role" >
-            <option dropdown="user" >user</option>
-            <option dropdown="admin" >admin</option>
-        </select>
-                    
-                    
-                    </ul> 
+
+            <div class="user-form input-padding">
+                Gebruikersnaam
+                <br><input type="text" name="username" minlength=5>
+            </div>
+
+
+            <div class="password-form input-padding">
+
+                <div class="password-form-initial">
+                    Wachtwoord <span class="info-symbol"><i class="fas fa-info-circle"></i></span>
+                    <br><input type="password" name="password" pattern="(?=.*\d)(?=.*[A-Z]).{8,}" title="minimaal: 8 karakters, 1 Hoofdletter, 1 Nummer" required>
                 </div>
-        
-    </div>     
-                    
-   
-            
-            <div class="menu_login">
-                <div id = "login_button">
-                    <ul>
-                        <input type="submit" value="Inloggen">
-                    </ul> 
+                <div class="password-form-confirm">
+                    Herhaal wachtwoord <br><input type="password" name="passwordconfirm">
                 </div>
             </div>
-        </form>
+
+            <div class="input-padding fullname-form">
+                <div class="fullname-form-fn">
+                    Voornaam
+                    <br><input type="text" name="firstname" minlength="2">
+                </div>
+                <div class="fullname-form-ln">
+                    Achternaam
+                    <br><input type="text" name="lastname" minlength="2">
+                </div>
+            </div>
+
+            <div class="input-padding email-form">
+                E-mailadres
+                <br><input type="email" name="email"><br>
+            </div>
+
+            <div class="role-form input-padding">
+                Rol
+                <br>
+                <select>
+                    <option value="user">gebruiker</option>
+                    <option value="admin">admin</option>
+                </select>
+            </div>
+
+    </div>
+
+    <!-- end form elements -->
+
+    <div class="footer"></div>
+
+    <!-- buttons   -->
+
+    <div class="footer-right">
+        <div class="buttons-form">
+            <button id="button-size" type="button">Annuleren</button><input id="button-size" type="submit" value="Gebruiker aanmaken">
+            <!-- buttons -->
+            <div>
+                </form>
+            </div>
+
+            <body>
+
+            </body>
+
+</html>
 
 <?php include ("../footer/footer.php"); ?>
 </body>
