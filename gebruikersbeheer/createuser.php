1e77cc9e813708f36e9d233be15f4084cc637ee3
Mitchell Sitohang <mitchellsitohang@me.com>
2018-09-14 10:24:25 +0200

    Aanpassingen aan pagina

--- gebruikersbeheer/createuser.php
+++ gebruikersbeheer/createuser.php
@@ -1,4 +1,5 @@
 <?php
+
 // ini_set('display_errors', 1);
     // Header in de bovenkant
     include ("../header/header.php");
@@ -14,37 +15,37 @@
     }
 
     // Kijk eerst of alle velden zijn ingevoerd met isset()
-    if( isset($_POST['username']) && isset($_POST['password']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['role']) )    
+    if( isset($_POST['username']) && isset($_POST['password']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['role']) )
     {
-        // Controleren of de user al bestaat 
+        // Controleren of de user al bestaat
         // Roep de class UserDaoMysql aan voor sql functionaliteit om user te checken
         $newUser = new UserDaoMysql();
         $newUser = $newUser->selectUser( $_POST['username'] );
-                
+
         // Haal de user info uit de User array/object $loginUser
         // en maak session vars aan.
         $_SESSION['username'] =  $newUser->getUsername();
-                
-        //Geef melding als de user al bestaat 
+
+        //Geef melding als de user al bestaat
         if( $_POST['username'] == $_SESSION['username'])
         {
             // Session leeg maken!!!!
             $_SESSION = array();
             echo "<br> <h2>Deze username bestaat al in de database.</h2>";
-        }        
-            
+        }
+
         // Controleren op hoofdletters
         if(!preg_match('/[A-Z]/', $_POST['password'] )){
             $_SESSION = array();
             echo "<br> <h2> Je moet minimaal een hoofdletter invoeren! </h2>";
         }
-        
+
         // Controleren op cijfers
         if (!preg_match('([0-9])', $_POST['password'] )){
             $_SESSION = array();
             echo "<br> <h2> Je moet minimaal een cijfer invoeren! </h2>";
         }
-        
+
         // Controleren of wachtwoorden gelijk zijn
         if( $_POST['password'] != $_POST['password2'] )
         {
@@ -52,14 +53,14 @@
             $_SESSION = array();
             echo "<br> <h2>Helaas... uw wachtwoord is niet gelijk....</h2>";
         }
-        
-        
+
+
         else
         {
             //encrypt het opgegeven password
             $encrypt = new EncryptDecrypt();
             $encrypt_password = $encrypt->encrypt($_POST['password']);
-            
+
             // Roep de class UserDaoMysql aan voor sql functionaliteit om user in te voeren in database
             $createUser = new UserDaoMysql();
             $createUser = $createUser->insertUser( $_POST['username'], $encrypt_password, $_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['role'] );
@@ -108,7 +109,7 @@
                     <br><input type="password" name="password" pattern="(?=.*\d)(?=.*[A-Z]).{8,}" title="minimaal: 8 karakters, 1 Hoofdletter, 1 Nummer" required>
                 </div>
                 <div class="password-form-confirm">
-                    Herhaal wachtwoord <br><input type="password" name="passwordconfirm" class="input-text-style">
+                    Herhaal wachtwoord <br><input type="password" name="password2" class="input-text-style">
                 </div>
             </div>
 
@@ -131,7 +132,7 @@
             <div class="role-form form-field-padding form-field-style">
                 Rol
                 <br>
-                <select>
+                <select required>
                     <optgroup label="Kies een rol">
                     <option selected hidden>Kies een rol</option>
                     <option value="user">gebruiker</option>
