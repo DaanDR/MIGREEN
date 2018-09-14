235a505b34a96abdf62826a3780a3ae1fe3b2a52
marsmar1 <henkiestyler@hotmail.com>
2018-09-14 14:17:57 +0200

    Updates om pagina voor klant toevoegen bij te voegen. Pagina is aanwezig, functionaliteit nog niet. Icon toegevoegd voor breadcrumbs volgens styleguide.

--- js/klantenoverzicht.js
+++ js/klantenoverzicht.js
@@ -1,13 +1,22 @@
 const newCustomerButton = document.getElementById("new-customer-button");
 const editButtons = document.getElementsByClassName("editbutton");
 const deleteButtons = document.getElementsByClassName("deletebutton");
+const createButton = document.getElementById("createButton");
+const removeButton = document.getElementById("removeButton");
+
+createButton.onclick = function(){
+	alert ('Nieuwe Klant Aanmaken');
+}
+
+removeButton.onclick = function(){
+	alert ('Klant verwijderen');
+}
 
 newCustomerButton.onclick = function() {
-	if (confirm('Wil je een nieuwe klant aanmaken?')) {
-		return true;
-	} else {
-		return false;
-	}
+	var customertable = document.getElementById("customerTable");
+	var createcustomer = document.getElementById("createCustomer");
+	customertable.style.display = "none";
+	createcustomer.style.display = "block";
 }
 
 
