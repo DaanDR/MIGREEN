b6418addadea1f80f96f99ede60b8e08bc1f24be
Huy Lam <huy.lam@hva.nl>
2018-09-17 16:52:54 +0200

    form.css + createuser.php opzet

--- gebruikersbeheer/createuser.php
+++ gebruikersbeheer/createuser.php
@@ -2,24 +2,24 @@
 session_start();
 
   // Check of user is ingelogged en anders terug naar de login pagina
-  include_once ("../autorisatie/UserIsLoggedin.php");
-  $userLoggedin = new UserIsLoggedin();
-  $userLoggedin->backToLoging();
+include_once ("../autorisatie/UserIsLoggedin.php");
+$userLoggedin = new UserIsLoggedin();
+$userLoggedin->backToLoging();
 
   // Check of de admin is ingelogged....
-  $adminLoggedin = "";
-  if( ! $userLoggedin->isAdmin() )
-  {
-      $adminLoggedin = "style='display: none;'";
-      echo "<br><br><br><br><h1>Geen gerbuikersrecht als admin.....</h1>";
-  }
+$adminLoggedin = "";
+if( ! $userLoggedin->isAdmin() )
+{
+  $adminLoggedin = "style='display: none;'";
+  echo "<br><br><br><br><h1>Geen gerbuikersrecht als admin.....</h1>";
+}
 
 // ini_set('display_errors', 1);
     // Header in de bovenkant
-    include ("../header/header.php");
+include ("../header/header.php");
 
     // Is logged in class
-    include_once ("../autorisatie/UserDaoMysql.php");
+include_once ("../autorisatie/UserDaoMysql.php");
     include ("../autorisatie/HashPassword.php"); // Hash PWD
 
     // Title van de pagina...
@@ -82,97 +82,115 @@ session_start();
             header('Location: ../gebruikersbeheer/overzicht.php');
         }
     }
-?>
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+    // customerDao voor selecteren van alle klanten
+    include ('../klantbeheer/CustomerDaoMysql.php');
 
-<head>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/form.css">
-    <link rel="stylesheet" href="../css/content.css">
+    $customerdaomysql = new CustomerDaoMysql();
+    $customers = $customerdaomysql-> selectAllCustomers();
+    ?>
 
-    <meta charset="utf-8">
-    <title>Gebruiker Aanmaken</title>
-</head>
-<body>
-<div class="grid-container" <?php echo $adminLoggedin ?> >
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
 
-    <div class="header-left">
-        <p class="breadcrumb">Home <i id="triangle-breadcrumb" class="fas fa-caret-right"></i> Gebruikersoverzicht</p>
-        <h2>Gebruiker aanmaken</h2>
-    </div>
+    <head>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/form.css">
+        <link rel="stylesheet" href="../css/content.css">
 
+        <meta charset="utf-8">
+        <title>Gebruiker Aanmaken</title>
+    </head>
+    <body>
+        <div class="grid-container" <?php echo $adminLoggedin ?> >
 
-    <div class="header"></div>
-
-    <!-- form elements -->
-
-    <div class="content">
-
-        <form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF'];?>">
-
-            <div class="user-form form-field-padding form-field-style">
-                Gebruikersnaam
-                <br><input type="text" name="username" minlength=5 class="input-text-style">
+            <div class="header-left">
+                <p class="breadcrumb">Home <i id="triangle-breadcrumb" class="fas fa-caret-right"></i> Gebruikersoverzicht</p>
+                <h2>Gebruiker aanmaken</h2>
             </div>
 
 
-            <div class="password-form form-field-padding form-field-style">
+            <div class="header"></div>
 
-                <div class="password-form-initial">
-                    Wachtwoord <span class="info-symbol password-info"><i class="fas fa-info-circle"></i><span class="password-infotext">Je wachtwoord moet minimaal bestaan uit:<p> 8 karakter met 1 hoofdletter en 1 nummer</p></span></span>
-                    <br><input type="password" name="password" pattern="(?=.*\d)(?=.*[A-Z]).{8,}" title="minimaal: 8 karakters, 1 Hoofdletter, 1 Nummer" required>
+            <!-- form elements -->
+
+            <div class="content">
+
+                <form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF'];?>">
+
+                    <div class="user-form form-field-padding form-field-style">
+                        Gebruikersnaam
+                        <br><input type="text" name="username" minlength=5 class="input-text-style">
+                    </div>
+
+
+                    <div class="password-form form-field-padding form-field-style">
+
+                        <div class="password-form-initial">
+                            Wachtwoord <span class="info-symbol password-info"><i class="fas fa-info-circle"></i><span class="password-infotext">Je wachtwoord moet minimaal bestaan uit:<p> 8 karakter met 1 hoofdletter en 1 nummer</p></span></span>
+                            <br><input type="password" name="password" pattern="(?=.*\d)(?=.*[A-Z]).{8,}" title="minimaal: 8 karakters, 1 Hoofdletter, 1 Nummer" required>
+                        </div>
+                        <div class="password-form-confirm">
+                            Herhaal wachtwoord <br><input type="password" name="password2" class="input-text-style">
+                        </div>
+                    </div>
+
+                    <div class="form-field-padding form-field-padding form-field-style">
+                        <div class="fullname-form-fn">
+                            Voornaam
+                            <br><input type="text" name="firstname" minlength="2" class="input-text-style">
+                        </div>
+                        <div class="fullname-form-ln">
+                            Achternaam
+                            <br><input type="text" name="lastname" minlength="2" class="input-text-style">
+                        </div>
+                    </div>
+
+                    <div class="form-field-padding form-field-style email-form">
+                        E-mailadres
+                        <br><input type="email" name="email" class="input-text-style"><br>
+                    </div>
+
+                    <div class="role-form form-field-padding form-field-style">
+                        Rol
+                        <br>
+                        <select name="role" required>
+                            <optgroup label="Kies een rol">
+                                <option selected hidden>Kies een rol</option>
+                                <option value="user">gebruiker</option>
+                                <option value="admin">admin</option>
+                            </optgroup>
+                        </select>
+                    </div>
+                    <div class="customer-form form-field-padding form-field-style">
+                        Gekoppelde klant(en)
+                        <br>
+                        <select name="customers" required>
+                            <optgroup label="Kies een klant">
+                                <option selected hidden>Kies een klant</option>
+                                <?php foreach($customers as $customer):?>
+                                    <option value="customer"><?=$customer["customerName"]?></option>
+                                    <?php endforeach;?>
+                            </optgroup>
+                        </select>
+                    </div>
+
                 </div>
-                <div class="password-form-confirm">
-                    Herhaal wachtwoord <br><input type="password" name="password2" class="input-text-style">
-                </div>
-            </div>
 
-            <div class="form-field-padding form-field-padding form-field-style">
-                <div class="fullname-form-fn">
-                    Voornaam
-                    <br><input type="text" name="firstname" minlength="2" class="input-text-style">
-                </div>
-                <div class="fullname-form-ln">
-                    Achternaam
-                    <br><input type="text" name="lastname" minlength="2" class="input-text-style">
-                </div>
-            </div>
+                <!-- end form elements -->
 
-            <div class="form-field-padding form-field-style email-form">
-                E-mailadres
-                <br><input type="email" name="email" class="input-text-style"><br>
-            </div>
+                <div class="footer"></div>
 
-            <div class="role-form form-field-padding form-field-style">
-                Rol
-                <br>
-                <select name="role" required>
-                    <optgroup label="Kies een rol">
-                    <option selected hidden>Kies een rol</option>
-                    <option value="user">gebruiker</option>
-                    <option value="admin">admin</option>
-                    </optgroup>
-                </select>
-            </div>
+                <!-- buttons   -->
 
-    </div>
-
-    <!-- end form elements -->
-
-    <div class="footer"></div>
-
-    <!-- buttons   -->
-
-    <div class="footer-right">
-        <div class="buttons-form">
-            <a href="overzicht.php" target="_self">
-            <button class="button-form-secondary" type="button">Annuleren</button></a>
-            <button class="button-form-primary" type="submit" value="Inloggen"> Gebruiker aanmaken </button>
-            <!-- buttons -->
-            <div>
-                </form>
-            </div>
-     </body>
-</html>
+                <div class="footer-right">
+                    <div class="buttons-form">
+                        <a href="overzicht.php" target="_self">
+                            <button class="button-form-secondary" type="button">Annuleren</button></a>
+                            <button class="button-form-primary" type="submit" value="Inloggen"> Gebruiker aanmaken </button>
+                            <!-- buttons -->
+                            <div>
+                            </form>
+                        </div>
+                    </body>
+                    </html>
