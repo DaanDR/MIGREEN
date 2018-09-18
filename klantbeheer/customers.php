b60df1c44f4b5a2a95859ebb32a2ef04a9856f3d
Daan de Rond <daanderond@hotmail.com>
2018-09-18 15:11:07 +0200

    uniform css file voor alle overzichten pages

--- klantbeheer/customers.php
+++ klantbeheer/customers.php
@@ -1,78 +1,72 @@
+<?php
+if (isset($_POST['customerName'])) {
+    if (strlen($_POST['customerName']) < 2) {
+        echo "<script type='text/javascript'>stringTooShort();</script>";
+    } else {
+        $createCustomer = $customerdaomysql->insertCustomer($_POST['customerName']);
+        header('Location: ../klantbeheer/klantenoverzicht.php');
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="../css/customers.css">
+    <link rel="stylesheet" type="text/css" href="../css/overzicht.css">
 </head>
 
 <?php
 include ('../klantbeheer/CustomerDaoMysql.php');
+include ('../header/header.php');
 
 $customerdaomysql = new CustomerDaoMysql();
 $customers = $customerdaomysql->selectAllCustomers();
 
 ?>
-<body>
-	<div id="pageheader"><?php
-include ('../header/header.php');
-?></div>
-	<div id="pagestyling">
-		<!-- Div voor de tabel met klantnamen -->
-		<div id="customerTable">
-			<table>
-				<thead>
-					<tr>
-						<th id="tabletitle">Home
-							<p>Klantoverzicht</p>
-						</th>
-						<th></th>
-						<th id="new-customer-button"><button class="new-customer-button"
-								type="button" name="button">Nieuwe klant aanmaken</button></th>
-					</tr>
-					<tr>
-						<th id="tablehead">KLANTNAAM</th>
-						<th></th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-    <?php foreach($customers as $client):?>
-    <tr class="withhover">
-						<td class='klantnaam'><?=$client["customerName"]?></td>
-						<td class='editbutton'><a
-							href="../klantbeheer/editcustomer.php?customer=<?php echo $client["customerName"]; ?>"><img
-							src='../res/edit.svg'><img src='../res/edit-hover.svg'></a></td>
-						<td class='deletebutton'><a onclick="return confirm('Wilt u klant <?php echo $client["customerName"] ?> echt verwijderen?');" 
-							href="../klantbeheer/customers.php?action=delete&customer=<?php echo $client["customerName"]; ?>"><img src='../res/delete.svg'><img
-							src='../res/delete-hover.svg'></a></td>
-					</tr>
-					</tr>
-    <?php endforeach;?>
-			</tbody>
-			</table>
-		</div>
-	</div>
-	<script src="../js/customers.js"></script>
-	<?php 
-	
-	if (! isset($_GET["action"])) {
-	    $action = "Home";
-	} else {
-	    $action = $_GET["action"];
-	}
-	
-	switch ($action) {
-	    case "Home":
-	        break;
-	    case "delete":
-	        if(isset($_GET['customer'])){
-	            
-	            $deleteCustomer = $customerdaomysql->deleteCustomer($_GET['customer']);
-	            echo '<script type="text/javascript"> customerDeleted(<?=$client["customerName"]?>); </script>';
-	            header("Location: ../klantbeheer/customers.php");
-	        }
-	        break;
-	}
-	?>
+<body id="overzicht-container">
+
+
+<div class="grid-container"  >
+    <div class="header-left">
+        <h1>Home</h1>
+        <h2>Klantoverzicht</h2>
+    </div>
+    <div class="header-mid"></div>
+    <div class="header-right">
+
+        <a href="createuser.php" target="_self">
+            <button class="new-user-button" type="button" name="button">Nieuwe klant aanmaken</button>
+        </a>
+    </div>
+    <div class="content">
+
+        <table>
+            <thead>
+            <tr>
+                <th>KLANTNAAM</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+
+
+            <?php foreach($customers as $client):?>
+                <tr>
+                    <td><?=$client["customerName"] ?></td>
+                    <td class="icon-cell" id="klant-icon-cell">
+                        <a href="../gebruikersbeheer/overzicht.php?action=edit&userName=<?php echo $client; ?>">
+                            <i class="editbutton"><img src='../res/edit.svg'><img
+                                        src='../res/edit-hover.svg'></i></a>
+                        <a href="../gebruikersbeheer/overzicht.php?action=delete&userName=<?php echo $client; ?>">
+                            <i class="deletebutton" onclick="return confirmDelete('<?php echo $client ?>');"><img src='../res/delete.svg'><img
+                                        src='../res/delete-hover.svg'></i></a>
+                    </td>
+                </tr>
+            <?php endforeach;?>
+
+            </tbody>
+    </div>
+</div>
+<script src="../js/customers.js"></script>
 </body>
 </html>
