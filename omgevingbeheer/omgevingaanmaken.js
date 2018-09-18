faec56cf6c78c7eec52e421b2e66a91168165a37
anderson petrus <anderson.petrus@hva.nl>
2018-09-18 13:25:55 +0200

    Update buttons

--- omgevingbeheer/omgevingaanmaken.js
+++ omgevingbeheer/omgevingaanmaken.js
@@ -14,9 +14,9 @@ function showTab(n) {
         document.getElementById("prevBtn").style.display = "inline";
     }
     if (n == (x.length - 1)) {
-        document.getElementById("nextBtn").innerHTML = "Submit";
+        document.getElementById("nextBtn").innerHTML = "Opslaan";
     } else {
-        document.getElementById("nextBtn").innerHTML = "Next";
+        document.getElementById("nextBtn").innerHTML = "Volgende";
     }
     //... and run a function that will display the correct step indicator:
     fixStepIndicator(n)
