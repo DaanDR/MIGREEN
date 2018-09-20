28b8282222e93af75721b7d62529763f3e75f0bb
Guido Leen <guido.leen@hva.nl>
2018-09-20 10:34:03 +0200

    Confirm logout popup

--- js/header.js
+++ js/header.js
@@ -1,4 +1,5 @@
-function deleteask(){
+function deleteask()
+{
   if (confirm('Wil je zeker weten uitloggen?')){
     return true;
   }else{
@@ -6,3 +7,25 @@ function deleteask(){
   }
 }
 
+document.getElementById("buttOkLogOut").addEventListener("click",
+function()
+{
+    window.location.href = document.getElementById("error_message_url").value;
+    
+   // "http://localhost/guido/mybit/migreen/header/header.php?action=logout&logoutinfo=1";
+});
+
+
+var objCancel = document.getElementById("buttCancel");
+objCancel.addEventListener("click",
+        function()
+        {
+            var objErrM = document.getElementsByClassName("error-message-container");
+            var objErrB = document.getElementsByClassName("error-message-backgr");
+
+            for (var index = 0; index < objErrM.length; index++) 
+            {
+                objErrM[index].style.display = "none";
+                objErrB[index].style.display = "none";
+            }
+    });
